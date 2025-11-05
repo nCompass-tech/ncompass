3671ac5062a712e31c13274558cbb3b7df964b0d
vineye25 <vinay.maniam@ncompass.tech>
2025-11-05 16:16:41 +0000

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,97 @@
+{
+  description = "nCompass Trace Tools";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs"; };
+
+  outputs = { self, nixpkgs } :
+    let
+      python_pkgs = pkgs : (with pkgs.python311Packages; [
+            pip
+            pudb
+          ]);
+
+      system_pkgs = pkgs : (with pkgs; [
+          git-lfs
+          pyright
+          plantuml
+        ]);
+
+      venv_name="venv-lsp";
+      venv_pip_pkgs = ''
+          set -e
+          
+          handle_reqs() {
+            mkdir -p reqs
+            pip freeze > nc_reqs.txt.tmp
+
+            if [ -f "nc_reqs.txt" ]; then
+              # Only backup if contents are different
+              if ! cmp -s "nc_reqs.txt" "nc_reqs.txt.tmp"; then
+                timestamp=$(date '+%Y-%m-%d_%H-%M')
+                cp nc_reqs.txt "reqs/nc_reqs.txt.$timestamp"
+                echo "Backed up existing nc_reqs.txt to reqs/nc_reqs.txt.$timestamp"
+                mv nc_reqs.txt.tmp nc_reqs.txt
+                echo "Updated nc_reqs.txt"
+              else
+                echo "No changes detected in nc_reqs.txt"
+                rm nc_reqs.txt.tmp
+              fi
+            else
+              mv nc_reqs.txt.tmp nc_reqs.txt
+              echo "Created initial nc_reqs.txt"
+            fi
+          }
+
+          python3 -m venv ${venv_name}
+          source ${venv_name}/bin/activate
+          pip install uv
+          
+          if [ -f "nc_reqs.txt" ]; then
+            uv pip install -r nc_reqs.txt
+          elif [ -f "requirements.txt" ]; then
+            uv pip install -r requirements.txt
+            handle_reqs
+          else
+            echo "No requirements file found (checked nc_reqs.txt and requirements.txt)"
+          fi
+          set +e
+
+          venv-req() {
+            handle_reqs
+          }
+          '';
+
+      free_pkgs_linux = import nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+        config.nvidia.acceptLicense = true;
+      };
+      
+      free_pkgs_osx = import nixpkgs {
+        system = "aarch64-darwin";
+        config.allowUnfree = true;
+      };
+      
+      linuxNixEnvPackages = pkgs : 
+        pkgs.mkShell {
+          buildInputs = (python_pkgs pkgs) ++ (system_pkgs pkgs);
+          shellHook = 
+          ''
+            export PS1="$( [[ -z $IN_NIX_SHELL ]] && echo "" || echo "[$(basename $PWD)]" ) $PS1"
+            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ 
+                                        pkgs.stdenv.cc.cc 
+                                    ]}:$LD_LIBRARY_PATH
+          '' + venv_pip_pkgs;
+        };
+      
+      osxNixEnvPackages = pkgs : 
+        pkgs.mkShell {
+          buildInputs = (python_pkgs pkgs) ++ (system_pkgs pkgs);
+          shellHook = venv_pip_pkgs;
+        };
+    
+    in {
+      devShell.aarch64-darwin = osxNixEnvPackages free_pkgs_osx;
+      devShell.x86_64-linux = linuxNixEnvPackages free_pkgs_linux;
+    };
+}
